c13d573135035d0745fab126cc9215ca446dce66
kylemh <kylemh.email@gmail.com>
2017-02-01 10:52:50 -0800

    Rename app/ to src/ and add wsgi file

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-	name = 'application_name',
-	packages = ['application_name'],
-	include_package_data = True,
-	install_requires = [flask, psycopg2] # TODO: Find out what to do between venv and setuptools or if theyre different things.
-)
