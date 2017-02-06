95aa61e017067484759e9ebb69b631da4cbd8ef1
kylemh <kylemh.email@gmail.com>
2017-02-06 15:24:27 -0800

    Add queries for each report and complete data validation

--- src/app.py
+++ src/app.py
@@ -10,11 +10,14 @@ app.secret_key = APP_SECRET_KEY
 
 
 # Database Query Function
-def db_query(sql_string, for_selection):
+def db_query(sql_string, for_selection, passed_data):
 	conn = psycopg2.connect(DB_LOCATION)
 	cur = conn.cursor()
 	print("The query being executed is", sql_string, "\n")
-	cur.execute(sql_string)
+	if passed_data is None:
+		cur.execute(sql_string)
+	else:
+		cur.execute(sql_string, passed_data)
 
 	if for_selection is True:
 		try:
@@ -48,6 +51,7 @@ def validate_date(date_string):
 		return date
 	except ValueError:
 		raise ValueError("Incorrect data format, should be MM/DD/YYYY")
+
 
 # Templates
 @app.route('/')
@@ -83,13 +87,13 @@ def logout():
 @app.route('/report_filter', methods=['GET', 'POST'])
 def report_filter():
 	# Get facilities list for drop-down menu
-	facilities_list = db_query('SELECT common_name FROM facilities;', True)
+	facilities_list = db_query('SELECT common_name FROM facilities;', True, None)
 
 	# If a form has been submitted
 	if request.method == 'POST':
-		# Getting Date
+		# Validate and pass date
 		try:
-			validate_date(request.form['filter_date'])
+			validated_date = validate_date(request.form['filter_date'])
 		except ValueError:
 			print(ValueError)
 			flash(ValueError)
@@ -98,11 +102,27 @@ def report_filter():
 
 		# Not filtering by facility...
 		if request.form['filter_facility'] == 'none':
-			return redirect(url_for('moving_inventory'))
+			moving_query = "SELECT assets.asset_tag, assets.description, f1.location as 'location1', f2.location as 'location2', convoys.arrive_dt, convoys.depart_dt" \
+						   "FROM assets" \
+						   "JOIN asset_on ON assets.asset_pk = asset_on.asset_fk" \
+						   "JOIN convoy ON asset_fk" \
+						   "JOIN facilities f1 ON convoy.src_fk = f1.facility_pk" \
+						   "JOIN facilities f2 ON convoy.dst_fk = f2.facility_pk" \
+						   "WHERE convoys.arrive_dt >= %d AND convoys.depart_dt <= %d"
+			moving_inventory_data = db_query(moving_query, for_selection=False, passed_data=validated_date)
+			return redirect(url_for('moving_inventory'), date=validated_date, data=moving_inventory_data)
 
 		# Filtering by facility...
 		else:
-			return redirect(url_for('facility_inventory'))
+			selected_facility = request.form['filter_facility']
+			facility_query = "SELECT facilities.fcode, facilities.location, assets.asset_tag, assets.description, asset_at.arrive_dt, asset_at.depart_dt" \
+							 "FROM facilities " \
+							 "JOIN asset_at ON facilities.facility_pk = asset_at.facility_fk" \
+							 "JOIN assets ON asset_at.asset_fk = assets.asset_pk" \
+							 "WHERE facilities.common_name = ''' + facility + "'' \
+							 "AND asset_at.arrive_dt >= %d AND asset_at.depart_dt <= %d;"
+			facility_inventory_data = db_query(facility_query, for_selection=False, passed_data=validated_date)
+			return redirect(url_for('facility_inventory'), facility=selected_facility, data=facility_inventory_data)
 
 	return render_template('report_filter.html', facilities_list=facilities_list)
 
@@ -129,15 +149,3 @@ def page_not_found(e):
 # Application Deployment
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8080, debug=True)
-
-
-# MARK: SQL Queries Complete
-"""
-SELECT facilities.fcode, facilities.location, assets.asset_tag, assets.description, asset_at.arrive_dt, asset_at.depart_dt
-FROM facilities
-JOIN asset_at ON facilities.facility_pk = asset_at.facility_fk
-JOIN assets ON asset_at.asset_fk = assets.asset_pk
-WHERE facilities.common_name = ''' + facility + "' AND asset_at.arrive_dt >= \'" + date + "\' AND asset_at.depart_dt <= \'" + date + "\';"
-
-SELECT
-"""
