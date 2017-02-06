5bbf9c7666adfceda8ef2f0e4a9f7545751eb74c
kylemh <kylemh.email@gmail.com>
2017-02-05 23:39:54 -0800

    Fix facility insertions - Attempt #2

--- sql/facilities_map.sql
+++ sql/facilities_map.sql
@@ -1,9 +1,9 @@
 -- Load the facilities table
 INSERT INTO facilities (fcode,common_name,location) VALUES ('MB005','Moonbase', 'The Moon');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('DC','The Capitol', 'Washington, DC');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('DC','Washington, DC', 'Washington, DC');
 INSERT INTO facilities (fcode,common_name,location) VALUES ('HQ','Headquarters', 'Eugene, OR');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('NC','National City, California', 'National City, CA');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('LANM','Los Alamos, New Mexico', 'Los Alamos, NM');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('SPNV','Sparks, Nevada', 'Sparks, NV');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('NC','National City', 'National City, CA');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('LANM','Los Alamos, NM', 'Los Alamos, NM');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('SPNV','Sparks, NV', 'Sparks, NV');
 INSERT INTO facilities (fcode,common_name,location) VALUES ('S300','Site 300', 'UNDISCLOSED');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('GRLK','Groom Lake, Nevada', 'Groom Lake, NV');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('GRLK','Groom Lake', 'Groom Lake, NV');
