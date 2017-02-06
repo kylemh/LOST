de1adf4720fcdbd08035b8d00c61169ec0fe26b6
kylemh <kylemh.email@gmail.com>
2017-02-06 02:05:47 -0800

    Move from lrg to small Flask structure and prep files for deployment test

--- src/app.py
+++ src/app.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template, request
-from config import DB_NAME, HOST, PORT, DB_LOCATION, DEBUG
-import sys
-import psycopg2
+from flask import Flask, render_template, request, url_for, redirect
+from datetime import datetime
+import sys, psycopg2
 
+from src.config import HOST, PORT, DEBUG, APP_SECRET_KEY, DB_LOCATION
 
 app = Flask(__name__)
+app.secret_key = str(APP_SECRET_KEY)
+
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+
 @app.route('/login')
 def login():
 	return render_template('login.html')
+
 
 @app.route('/report_menu')
 def report_menu():
@@ -24,17 +28,26 @@ def report_menu():
 	# return render_template('report_menu.html', processed_data=processed_data)
 	return render_template('report_menu.html')
 
-@app.route('/facility_inventory_report')
-def facility_inventory_report():
-	return render_template('facility_inventory_report.html',facility=request.args.get('facility'),date=request.args.get('report_date'))
 
-@app.route('/in_transit_report')
+@app.route('/report_filter')
+def report_filter():
+	return render_template('report_filter.html')
+
+
+@app.route('/facility_inventory')
+def facility_inventory_report():
+	return render_template('facility_inventory.html', facility=request.args.get('facility'), date=request.args.get('report_date'))
+
+
+@app.route('/moving_inventory')
 def in_transit_report():
-	return render_template('in_transit_report.html',date=request.args.get('report_date'))
+	return render_template('moving_inventory.html', date=request.args.get('report_date'))
+
 
 @app.route('/logout')
 def logout():
 	return render_template('logout.html')
 
+
 # if __name__ == "__main__":
-# 	app.run(host='0.0.0.0',port=8080)
+# 	app.run(host = config.DB_LOCATION, port = config.PORT, debug = config.DEBUG)
--- src/config.py
+++ src/config.py
@@ -2,14 +2,13 @@ import json
 import os
 import pathlib
 
-
-"""
+'''
 NOTE:
 1) the APP_SECRET_KEY is secret. it should be generated in a way that makes everyone comfortable.
-   possible suggestions: A.) import os; os.urandom(size_bytes) B.) import uuid; str(uuid.uuid4())
+possible suggestions: A.) import os; os.urandom(size_bytes) B.) import uuid; str(uuid.uuid4())
 
 2) DB_LOCATION is a string that can be passed to psycopg2 to connect to the db
-"""
+'''
 
 cpath = pathlib.Path(os.path.realpath(__file__)).parent.joinpath('config.base.json')
 
@@ -19,6 +18,6 @@ with cpath.open() as conf:
 	HOST = c['database']['dbhost']
 	PORT = c['database']['dbport']
 
-APP_SECRET_KEY = ""  #TODO: string, see note 1 above
-DB_LOCATION = "0.0.0.0" #TODO: string, see note 2 above
+APP_SECRET_KEY = ""  # TODO: string, see note 1 above
+DB_LOCATION = "0.0.0.0"  # TODO: string, see note 2 above
 DEBUG = True
