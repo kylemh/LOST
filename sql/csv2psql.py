3164b6656a2a6597894f9c85b334e25dbae6ada8
kylemh <kylemh.email@gmail.com>
2017-02-05 21:40:09 -0800

    Add ability for converted to deal with security and product data

--- sql/csv2psql.py
+++ sql/csv2psql.py
@@ -3,8 +3,6 @@ import csv
 import sys
 import glob
 
-# TODO: Implement more extensible format
-
 def main():
 	# Connect to database
 	conn = "host='localhost' port='" + PORT + "' dbname='" + DB_NAME + "' user='osnapdev' password='secret'"
@@ -12,28 +10,69 @@ def main():
 	db = psycopg2.connect(conn)
 	print("CONNECTION COMPLETE")
 
-	# Create database cursor
-	global CUR
+	# Instantiate database cursor
 	CUR = db.cursor()
 
 	# Increase work memory
 	work_mem = 1024
 	CUR.execute('SET work_mem TO ' + str(work_mem))
 
-	# Translate legacy data into useful information for facilities and inventory for each facility
-	facilities()
+	# Translate legacy data into useful information
+	# Security Data
+	with open('sec_load.sql', 'w') as f:
+		process_levels(f)
+		process_compartments(f)
 
+	# Facilities and Inventory Data
+	facilities() #inventory() gets called within
+
+	# Product Data
+	with open('prod_load.sql', 'w') as f:
+		process_products(f)
+
+	with open('prod_load.sql', 'w') as f:
+		process_products(f)
+
+
+	# Inventory Data
 	# TODO: Populate Products, Levels, Compartments, and Security Tags
 
 	# Commit changes to database
 	db.commit()
 
 
+############################
+# SECURITY DATA PROCESSING #
+############################
+def process_levels(outf):
+	with open('osnap_legacy/security_levels.csv') as f:
+		data = csv.DictReader(f)
+		i = 5
+		for r in data:
+			outf.write("INSERT INTO sec_levels (level_pk,abbrv,comment) VALUES (%s,'%s','%s');\n"%(i,r['level'],r['description']))
+			i = i-1
+
+def process_compartments(outf):
+	with open('osnap_legacy/security_compartments.csv') as f:
+		data = csv.DictReader(f)
+		for r in data:
+			outf.write("INSERT INTO sec_compartments (abbrv,comment) VALUES ('%s','%s');\n"%(r['compartment_tag'],r['compartment_desc']))
+
+
+########################################
+# FACILITY & INVENTORY DATA PROCESSING #
+########################################
 def facilities():
+	# Empty Facilities
+	insert("facilities", ["fcode", "common_name", "location"], ["S300", "Site 300", "UNDISCLOSED"])
+	insert("facilities", ["fcode", "common_name", "location"], ["GRLK", "Groom Lake, Nevada", "Groom Lake, NV"])
+	insert("facilities", ["fcode", "common_name", "location"], ["GRLK", "Los Alamos, New Mexico", "Los Alamos, NM"])
+
+	# Facilities With Inventory
 	for fn in glob.iglob('./osnap_legacy/**/*.csv', recursive=True):
 		if "inventory" in fn:
 			fcode = fn.split('/')[2].split('_')[0]
-			print("\nTHIS IS THE FCODE:", fcode, "\n")
+
 			# Translate facility code into understandable data
 			if fcode == "HQ":
 				common_name = "Headquarters"
@@ -42,20 +81,19 @@ def facilities():
 				common_name = "The Capitol"
 				location = "Washington, D.C."
 			elif fcode == "SPNV":
-				common_name = "Sparks Nevada"
+				common_name = "Sparks, Nevada"
 				location = "Sparks, NV"
 			elif fcode == "NC":
 				common_name = "National City"
 				location = "National City, CA"
-			else:
-				common_name = fcode
-				location = "UNDISCLOSED"
+			elif fcode == "MB005":
+				common_name = "Moonbase"
+				location = "Earth's Moon"
 
 			insert("facilities", ["fcode", "common_name", "location"], [fcode, common_name, location])
 			CUR.execute("SELECT facility_pk FROM facilities WHERE fcode = '" + fcode + "';")
 			facility_pk = CUR.fetchone()[0]
 			inventory(fn, facility_pk)
-
 
 def inventory(fn, facility_fk):
 	reader = csv.reader(open(fn, newline=''), delimiter=',', quotechar='|')
@@ -66,11 +104,38 @@ def inventory(fn, facility_fk):
 		asset_fk = CUR.fetchone()[0]
 		insert("asset_at", ["asset_fk", "facility_fk"], [asset_fk, facility_fk])
 
+################
+# PRODUCT DATA #
+################
+def process_products(outf):
+	with open('osnap_legacy/product_list.csv') as f:
+		data = csv.DictReader(f)
+		for r in data:
+			for k in r.keys():
+				print("%s: %s" % (k, r[k]))
+				if r[k] == '':
+					r[k] = 'NULL'
+				elif k == 'unit price':
+					pass
+				else:
+					r[k] = "'%s'" % r[k]  # if single quotes are in the data this will fail
 
-# TODO: Define get_pk(table, columns, values)
-def get_pk(table, columns, values):
-	# UNIMPLEMENTED
-	return
+			# Add the product entry
+			outf.write("INSERT INTO products (vendor,product_name,product_model,description,price) VALUES (%s,%s,%s,%s,%s);\n" % (
+			r['vendor'], r['name'], r['model'], r['description'], r['unit price']))
+
+			# Add the security tag if there is one
+			if not r['compartments'] == 'NULL':
+				(tc, tl) = r['compartments'].split(':')
+
+			'''
+			This query exploits how serial values are generated and may fail
+			if something else is running at the same time as the migration script.
+			This query is also likely to be super inefficient for large migrations.
+			'''
+			outf.write("INSERT INTO security_tags (level_fk, compartment_fk, product_fk) "
+					   "SELECT level_pk, compartment_pk, max(product_pk) FROM sec_levels l,sec_compartments c,products p "
+					   "WHERE l.abbrv='%s and c.abbrv=%s' GROUP BY level_pk,compartment_pk,product_pk;\n" % (tl, tc))
 
 
 def insert(table, columns, values):
@@ -93,6 +158,7 @@ def insert(table, columns, values):
 if __name__ == '__main__':
 	global DB_NAME
 	global PORT
+	global CUR
 
 	try:
 		DB_NAME = sys.argv[1]
@@ -103,8 +169,6 @@ if __name__ == '__main__':
 		DB_NAME = 'lost'
 		PORT = '5432'
 	else:
-		print("\nYou entered incorrect arguments...\n")
-		DB_NAME = 'lost'
-		PORT = '5432'
+
 
 	main()
--- sql/import_data.sh
+++ sql/import_data.sh
@@ -2,6 +2,10 @@
 db_name=$1
 port=$2
 
+
+#############################
+# CREATE DB & GET CSV FILES #
+#############################
 printf '\e[1;34mCreating empty tables for database model...\n\e[0m'
 psql lost < create_tables.sql
 
@@ -12,8 +16,28 @@ printf '\e[1;34mUnzipping compressed data...\n\e[0m'
 tar -xvf osnap_legacy.tar.gz
 rm ./osnap_legacy/.*.csv
 
+
+#############################
+# CONVERT CSV TO POSTGRESQL #
+#############################
 printf '\e[1;34mAttempting to populate database from legacy data...\n\e[0m'
 python3 csv2psql.py $db_name $port
+
+
+##################################################
+# Piping in and cleaning up generated .sql files #
+##################################################
+# Security Data
+psql $1 -f sec_load.sql
+rm sec_load.sql
+
+# Product Data
+psql $1 -f prod_load.sql
+rm prod_load.sql
+
+#
+
+
 
 printf '\e[1;34mGarbage collection taking place...\n\e[0m'
 rm -rf osnap_legacy
