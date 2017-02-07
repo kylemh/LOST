54b11cfd0f81560b652742c6f88b595726748b61
kylemh <kylemh.email@gmail.com>
2017-02-06 19:34:08 -0800

    Fix queries - Attempt #4

--- src/app.py
+++ src/app.py
@@ -103,8 +103,8 @@ def report_filter():
 						   " FROM assets" \
 						   " JOIN asset_on ON assets.asset_pk = asset_on.asset_fk" \
 						   " JOIN convoy ON asset_fk" \
-						   " JOIN facilities f1 ON convoy.src_fk = f1.facility_pk" \
-						   " JOIN facilities f2 ON convoy.dst_fk = f2.facility_pk" \
+						   " JOIN facilities f1 ON convoys.src_fk = f1.facility_pk" \
+						   " JOIN facilities f2 ON convoys.dst_fk = f2.facility_pk" \
 						   " WHERE convoys.arrive_dt >= '%s' AND convoys.depart_dt <= '%s'" % (validated_date, validated_date)
 
 			moving_inventory_data = db_query(moving_query, for_selection=False)
