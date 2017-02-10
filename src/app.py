2c97221cc9570600c11d5327f9ab8c6f8224b7ac
kylemh <kylemh.email@gmail.com>
2017-02-09 18:24:49 -0800

    Add notes and attempt fix to data rendering issue

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 """
 TODO: Implement other error handlers - http://flask.pocoo.org/docs/0.12/patterns/errorpages/
 TODO: Shit-loads of refactoring
+TODO: Proper Error Handling of Entries in report_filter()
 """
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
@@ -23,18 +24,18 @@ def db_query(sql_string, data_array):
 	cur.execute(sql_string, data_array)
 
 	# Return data as an array of dictionaries
-	records = []
 	result = cur.fetchall()
+	records = []
 
 	# If the query returns something...
 	if len(result) != 0:
 		entries = result
-
+		print("\nLine 33 - entries:", entries)
 		for row in entries:
 			records.append(row)
 	else:
 		# No results in query
-		print("\n\bNO RESULTS IN QUERY\n")
+		print('\n\bNO RESULTS IN QUERY\n')
 		redirect(url_for('failed_query', query_string=sql_string))
 
 	conn.commit()
@@ -49,7 +50,7 @@ def validate_date(date_string):
 		date = datetime.datetime.strptime(date_string, '%m/%d/%Y').date()
 		return str(date)
 	except ValueError:
-		raise ValueError("Incorrect data format, should be MM/DD/YYYY")
+		raise ValueError('Incorrect data format, should be MM/DD/YYYY')
 
 
 # MARK: TEMPLATES
@@ -64,9 +65,9 @@ def index():
 def login():
 	error = None
 	if request.method == 'POST':
-		if False and request.form['username'] != USERNAME:
+		if False or request.form['username'] != USERNAME:
 			error = 'Invalid username'
-		elif False and request.form['password'] != PASSWORD:
+		elif False or request.form['password'] != PASSWORD:
 			error = 'Invalid password'
 		else:
 			session['logged_in'] = True
@@ -106,12 +107,13 @@ def report_filter():
 		try:
 			validated_date = validate_date(request.form['filter_date'])
 		except ValueError:
-			print("\n\nValueError.args is:", ValueError.args, "\n\n")
-			flash(ValueError.args)
+			print('VALUE ERROR:', ValueError.args)
+			pass
 		except TypeError or UnboundLocalError:
-			flash("You need to enter a date.")
+			print('TYPE ERROR:', 'You need to enter a date.')
+			pass
 
-		# FILTER: Inventory by "In Transit" status
+		# FILTER: Inventory by 'In Transit' status
 		if request.form['filter_facility'] == 'none':
 			return moving_inventory(validated_date)
 
@@ -122,15 +124,43 @@ def report_filter():
 	return render_template('report_filter.html', facilities_list=facilities_list)
 
 
+@app.route('/moving_inventory', methods=['GET', 'POST'])
+def moving_inventory(validated_date):
+	moving_query = 'SELECT assets.asset_tag, assets.description, f1.location as location1, f2.location as location2, convoys.depart_dt, convoys.arrive_dt' \
+				   ' FROM assets' \
+				   ' JOIN asset_on ON assets.asset_pk = asset_on.asset_fk' \
+				   ' JOIN convoys ON asset_on.convoy_fk = convoys.convoy_pk' \
+				   ' JOIN facilities f1 ON convoys.src_fk = f1.facility_pk' \
+				   ' JOIN facilities f2 ON convoys.dst_fk = f2.facility_pk' \
+				   ' WHERE convoys.arrive_dt >= %s AND convoys.depart_dt <= %s'
+
+	moving_inventory_data = db_query(moving_query, [validated_date, validated_date])
+	print('Line 138 - moving_enventory_data:', moving_inventory_data)
+	column_names = ['asset_tag', 'description', 'location1', 'location2', 'depart_dt', 'arrive_dt']
+	moving_inventory_processed = []
+
+	# TODO: Refactor by creating array of tuples to dictionary conversion function
+	# If list is not empty and it's size matches the array of column headers
+	if moving_inventory_data and (len(moving_inventory_data[0]) == len(column_names)):
+		print('\nNot Empty!\n')
+		for record in moving_inventory_data:
+			moving_inventory_processed.append(dict(zip(column_names, record)))
+	else:
+		print('\n\n\n ERROR LIST OF COLUMN SIZE IS NOT THE SAME SIZE AS RECORD SIZE \n\n\n')
+
+	print('\nThis is the data being passed:', moving_inventory_processed)
+	return render_template('moving_inventory.html', date=validated_date, data=moving_inventory_processed)
+
+
 @app.route('/facility_inventory', methods=['GET', 'POST'])
 def facility_inventory(validated_date):
 	selected_facility = str(request.form['filter_facility'])
-	facility_query = "SELECT facilities.fcode, facilities.location, assets.asset_tag, assets.description, asset_at.arrive_dt, asset_at.depart_dt" \
-					 " FROM facilities" \
-					 " JOIN asset_at ON facilities.facility_pk = asset_at.facility_fk" \
-					 " JOIN assets ON asset_at.asset_fk = assets.asset_pk" \
-					 " WHERE facilities.common_name = %s" \
-					 " AND asset_at.arrive_dt >= %s AND asset_at.depart_dt >= %s;"
+	facility_query = 'SELECT facilities.fcode, facilities.location, assets.asset_tag, assets.description, asset_at.arrive_dt, asset_at.depart_dt' \
+					 ' FROM facilities' \
+					 ' JOIN asset_at ON facilities.facility_pk = asset_at.facility_fk' \
+					 ' JOIN assets ON asset_at.asset_fk = assets.asset_pk' \
+					 ' WHERE facilities.common_name = %s' \
+					 ' AND asset_at.arrive_dt >= %s AND asset_at.depart_dt >= %s;'
 
 	facility_inventory_data = db_query(facility_query, [selected_facility, validated_date, validated_date])
 
@@ -139,40 +169,13 @@ def facility_inventory(validated_date):
 
 	# TODO: Refactor by creating array of tuples to dictionary conversion function
 	# If list is not empty and it's size matches the array of column headers
-	if facility_inventory_data and ((facility_inventory_data[0]) == len(column_names)):
+	if facility_inventory_data and (len(facility_inventory_data[0]) == len(column_names)):
 		for record in facility_inventory_data:
 			facility_inventory_processed.append(dict(zip(column_names, record)))
 	else:
-		print("\n\n\n ERROR LIST OF COLUMN SIZE IS NOT THE SAME SIZE AS RECORD SIZE \n\n\n")
+		print('\n\n\n ERROR LIST OF COLUMN SIZE IS NOT THE SAME SIZE AS RECORD SIZE \n\n\n')
 
 	return render_template('facility_inventory.html', facility=selected_facility, data=facility_inventory_processed, date=validated_date)
-
-
-@app.route('/moving_inventory', methods=['GET', 'POST'])
-def moving_inventory(validated_date):
-	moving_query = "SELECT assets.asset_tag, assets.description, f1.location as location1, f2.location as location2, convoys.depart_dt, convoys.arrive_dt" \
-				   " FROM assets" \
-				   " JOIN asset_on ON assets.asset_pk = asset_on.asset_fk" \
-				   " JOIN convoys ON asset_on.convoy_fk = convoys.convoy_pk" \
-				   " JOIN facilities f1 ON convoys.src_fk = f1.facility_pk" \
-				   " JOIN facilities f2 ON convoys.dst_fk = f2.facility_pk" \
-				   " WHERE convoys.arrive_dt >= %s AND convoys.depart_dt <= %s"
-
-	moving_inventory_data = db_query(moving_query, [validated_date, validated_date])
-
-	column_names = ['asset_tag', 'description', 'location1', 'location2', 'depart_dt', 'arrive_dt']
-	moving_inventory_processed = []
-
-	# TODO: Refactor by creating array of tuples to dictionary conversion function
-	# If list is not empty and it's size matches the array of column headers
-	if moving_inventory_data and ((moving_inventory_data[0]) == len(column_names)):
-		for record in moving_inventory_data:
-			moving_inventory_processed.append(dict(zip(column_names, record)))
-	else:
-		print("\n\n\n ERROR LIST OF COLUMN SIZE IS NOT THE SAME SIZE AS RECORD SIZE \n\n\n")
-
-	print("\nThis is the data being passed:", moving_inventory_processed)
-	return render_template('moving_inventory.html', date=validated_date, data=moving_inventory_processed)
 
 
 @app.route('/facility_inventory', methods=['GET'])
@@ -187,5 +190,5 @@ def page_not_found(e):
 
 
 # Application Deployment
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080, debug=True)
