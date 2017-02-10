5998099a10fbfb41af8a27c0e74b26b5b689a067
kylemh <kylemh.email@gmail.com>
2017-02-10 00:00:13 -0800

    Fix facility query - Attempt #17

--- src/app.py
+++ src/app.py
@@ -158,8 +158,8 @@ def facility_inventory(validated_date):
 					 ' FROM (SELECT * FROM facilities WHERE common_name = %s) as facilities' \
 					 ' JOIN asset_at ON facilities.facility_pk = asset_at.facility_fk' \
 					 ' JOIN assets ON asset_at.asset_fk = assets.asset_pk' \
-					 ' WHERE (asset_at.depart_dt <= %s OR asset_at.depart_dt IS NULL)' \
-					 ' AND asset_at.arrive_dt >= %s;'
+					 ' WHERE (asset_at.depart_dt >= %s OR asset_at.depart_dt IS NULL)' \
+					 ' AND asset_at.arrive_dt <= %s;'
 
 	facility_inventory_data = db_query(facility_query, [selected_facility, validated_date, validated_date])
 	print("\n\nLine 166 - facility_inventory_data:", facility_inventory_data)
