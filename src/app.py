6cdcbde49bcad46d1f27a62a227ef25efbfc69b7
Kyle Holmberg <kylemh.email@gmail.com>
2017-02-27 11:58:22 +0000

    Implement add_facility screen

--- src/app.py
+++ src/app.py
@@ -19,7 +19,7 @@ app.secret_key = APP_SECRET_KEY
 
 # MARK: DATABASE FUNCTIONS
 def db_query(sql_string, data_list):
-	"""This function is designed to take a SQL query string and a list of data to be used (in order) as injection-safe, variable fill-ins for the query."""
+	"""This function is designed to take a SQL query string and a list of data to be used - in order - as injection-safe, variable fill-ins for the query."""
 	conn = psycopg2.connect(dbname=DB_NAME, host=HOST, port=PORT)
 	cur = conn.cursor()
 	cur.execute(sql_string, data_list)
@@ -49,12 +49,26 @@ def db_insert(sql_string, data_list):
 	cur = conn.cursor()
 	try:
 		cur.execute(sql_string, data_list)
-	except e:
+	except:
 		print("INSERTION FAILED")
 
 	conn.commit()
 	cur.close()
 	conn.close()
+
+
+def duplicate_check(sql_string, data_list):
+	"""This function is designed to return True if a query yields a result and False if not."""
+	conn = psycopg2.connect(dbname=DB_NAME, host=HOST, port=PORT)
+	cur = conn.cursor()
+	cur.execute(sql_string, data_list)
+	result = cur.fetchall()
+	cur.close()
+	conn.close()
+	if result:
+		return True
+	else:
+		return False
 
 
 # MARK: TEMPLATES
@@ -83,6 +97,7 @@ def login():
 		else:
 			check_for_user = "SELECT * FROM users WHERE username = %s;"
 			result = db_query(check_for_user, [username])
+			# Result is ([user_pk, role_fk, username, password])
 
 			if result is None:
 				# User DOES NOT exist:
@@ -94,6 +109,7 @@ def login():
 					# Password is correct
 					session['username'] = username
 					session['logged_in'] = True
+					session['perms'] = result[0][1]
 					welcome_message = 'Welcome ' + str(session.get('username')) + '!'
 					flash(welcome_message)
 					return redirect('/dashboard')
@@ -129,35 +145,62 @@ def create_user():
 
 		if username is None:
 			flash('Please enter a username.')
-			return render_template('create_user.html')
 		elif password is None:
 			flash('Please enter a password.')
-			return render_template('create_user.html')
 		else:
 			# Form was completed
-			check_for_username = "SELECT user_pk FROM users WHERE username = %s;"
-			response = db_query(check_for_username, [username])
+			matching_user = "SELECT user_pk FROM users WHERE username = %s;"
+			user_does_exist = duplicate_check(matching_user, [username])
 
-			if response is None:
-				# User does not already exist - create it
-				add_username = "INSERT INTO users (username, password, role_fk) VALUES (%s, %s, %s);"
-				db_insert(add_username, [username, password, role])
-				flash('Your account was created!')
-			else:
+			if user_does_exist:
 				flash('Username already exists')
+			else:
+				# User does not already exist - create it
+				new_user = "INSERT INTO users (username, password, role_fk) VALUES (%s, %s, %s);"
+				db_insert(new_user, [username, password, role])
+				flash('Your account was created!')
 
 	return render_template('create_user.html')
 
 
-@app.route('/failed_query', methods=['GET'])
-def failed_query(query_string):
-	return render_template('failed_query.html', query=query_string)
+@app.route('/add_facility', methods=['GET', 'POST'])
+def add_facility():
+	if request.method == 'POST':
+		fcode = request.form.get('fcode', None)
+		common = request.form.get('common', None)
+		location = request.form.get('location', None)
+
+		if fcode is None or common is None or location is None:
+			flash('Please complete the form')
+		else:
+			matching_facilities = "SELECT facility_pk FROM facilities WHERE fcode=%s OR common_name=%s;"
+			facility_does_exist = duplicate_check(matching_facilities, [fcode, common])
+
+			if facility_does_exist:
+				flash('There already exists a facility with that fcode or common name!')
+			else:
+				# Facility does not already exist - create it
+				new_facility = "INSERT INTO facilities (fcode, common_name, location) VALUES (%s, %s, %s);"
+				db_insert(new_facility, [fcode, common, location])
+				flash('New facility was created!')
+
+	# Get all current facilities for table population
+	all_facilities = "SELECT * FROM facilities;"
+	data = db_query(all_facilities, [])
+	print(data)
+
+	return render_template('add_facility.html', data=data)
 
 
 # MARK: ERROR PAGES
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
+
+
+@app.route('/failed_query', methods=['GET'])
+def failed_query(query_string):
+	return render_template('failed_query.html', query=query_string)
 
 
 # Application Deployment
