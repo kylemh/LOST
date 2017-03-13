e4cb6e64426b24b4f4ea37ad8f5b10a3e17e653f
Kyle Holmberg <kylemh.email@gmail.com>
2017-03-13 09:56:59 +0000

    Fix test data for table creation script.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -61,23 +61,26 @@ INSERT INTO users (role_fk, username, password) VALUES (2, 'log', 'abc');
 INSERT INTO users (role_fk, username, password) VALUES (3, 'fac', 'abc');
 INSERT INTO users (role_fk, username, password) VALUES (1, 'illegal', 'abc');
 
-INSERT INTO facilities (fcode,common_name,location) VALUES ('MB005','Moonbase','The Moon');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('DC','Washington, DC','Washington, DC');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('HQ','Headquarters','Cheyyene Mountain Complex, CO');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('NC','National City','National City, CA');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('LANM','Los Alamos, NM','Los Alamos, NM');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('SPNV','Sparks, NV','Sparks, NV');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('S300','Site 300','Area 51, NV');
-INSERT INTO facilities (fcode,common_name,location) VALUES ('GRLK','Groom Lake','Groom Lake, NV');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('MB005', 'Moonbase', 'The Moon');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('DC', 'Washington, DC', 'Washington, DC');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('HQ', 'Headquarters', 'Cheyyene Mountain Complex, CO');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('NC', 'National City', 'National City, CA');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('LANM', 'Los Alamos, NM', 'Los Alamos, NM');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('SPNV', 'Sparks, NV', 'Sparks, NV');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('S300', 'Site 300', 'Area 51, NV');
+INSERT INTO facilities (fcode,common_name,location) VALUES ('GRLK', 'Groom Lake', 'Groom Lake, NV');
 
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X001A','Alien skin samples', 'The Moon');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X002A','Alien hair folicles', 'The Moon');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X003A','Alien toenails', 'The Moon');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X001A','Alien skin samples', 'Sparks, NV');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X020H','Alien armor', 'Eugene, OR');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X001A','Alien skin samples', 'Sparks, NV');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('Z020A','Aid package', 'National City, CA');
-INSERT INTO assets (asset_tag, description, disposed) VALUES ('X100A','UFO Chassis', 'Area 51, NV');
+INSERT INTO assets (asset_tag, description, disposed) VALUES ('X001A', 'Alien skin samples', FALSE);
+INSERT INTO assets (asset_tag, description, disposed) VALUES ('X002A', 'Alien hair follicles', FALSE);
+INSERT INTO assets (asset_tag, description, disposed) VALUES ('X003A', 'Alien toenails', FALSE);
+INSERT INTO assets (asset_tag, description, disposed) VALUES ('X020H', 'Alien armor', FALSE);
+INSERT INTO assets (asset_tag, description, disposed) VALUES ('Z020A', 'Aid package', FALSE);
+INSERT INTO assets (asset_tag, description, disposed) VALUES ('X100A', 'UFO Chassis', FALSE);
 
-
+INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt, depart_dt) VALUES (1, 1, '2017-01-15 00:00:00', NULL);
+INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt, depart_dt) VALUES (2, 1, '2017-01-16 00:00:00', NULL);
+INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt, depart_dt) VALUES (3, 1, '2017-01-17 00:00:00', NULL);
+INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt, depart_dt) VALUES (4, 5, '2017-01-30 00:00:00', NULL);
+INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt, depart_dt) VALUES (5, 8, '2017-02-05 00:00:00', NULL);
+INSERT INTO asset_at (asset_fk, facility_fk, arrive_dt, depart_dt) VALUES (6, 7, '2017-02-15 00:00:00', NULL);
 
