cce6dd799137e2b015f17abdb20a8f83397f4f1f
Kyle Holmberg <kylemh.email@gmail.com>
2017-03-16 20:48:36 +0000

    Fixes so that WGSI deployment works for class

--- src/app.py
+++ src/app.py
@@ -767,4 +767,4 @@ def failed_query(query):
 
 # Application Deployment
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- src/config.py
+++ src/config.py
@@ -3,35 +3,12 @@ import os
 import pathlib
 
 
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    APP_SECRET_KEY = str(os.urandom(32))
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+APP_SECRET_KEY = str(os.urandom(32))
 
-    # Global Variables
-    basedir = pathlib.Path(os.path.realpath(__file__)).parent.joinpath('lost_config.json')
-    with basedir.open() as conf:
-        c = json.load(conf)
-        DB_NAME = c['database']['dbname']
-        HOST = c['database']['dbhost']
-        PORT = c['database']['dbport']
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-# class DevelopmentConfig(Config):
-# 	DEVELOPMENT = True
-# 	DEBUG = True
-#
-#
-# class TestingConfig(Config):
-# 	TESTING = True
+# Global Variables
+basedir = pathlib.Path(os.path.realpath(__file__)).parent.joinpath('lost_config.json')
+with basedir.open() as conf:
+    c = json.load(conf)
+    DB_NAME = c['database']['dbname']
+    HOST = c['database']['dbhost']
+    PORT = c['database']['dbport']
