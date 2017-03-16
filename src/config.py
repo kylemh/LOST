e962b0f4b7cd16c8c8142e0a3feb63d8a79ead2f
kylemh <kylemh.email@gmail.com>
2017-03-16 11:44:40 -0700

    Convert tabs to spaces

--- src/config.py
+++ src/config.py
@@ -4,28 +4,28 @@ import pathlib
 
 
 class Config(object):
-	DEBUG = False
-	TESTING = False
-	CSRF_ENABLED = True
-	APP_SECRET_KEY = str(os.urandom(32))
-	# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    APP_SECRET_KEY = str(os.urandom(32))
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-	# Global Variables
-	basedir = pathlib.Path(os.path.realpath(__file__)).parent.joinpath('lost_config.json')
-	with basedir.open() as conf:
-		c = json.load(conf)
-		DB_NAME = c['database']['dbname']
-		HOST = c['database']['dbhost']
-		PORT = c['database']['dbport']
+    # Global Variables
+    basedir = pathlib.Path(os.path.realpath(__file__)).parent.joinpath('lost_config.json')
+    with basedir.open() as conf:
+        c = json.load(conf)
+        DB_NAME = c['database']['dbname']
+        HOST = c['database']['dbhost']
+        PORT = c['database']['dbport']
 
 
 class ProductionConfig(Config):
-	DEBUG = False
+    DEBUG = False
 
 
 class StagingConfig(Config):
-	DEVELOPMENT = True
-	DEBUG = True
+    DEVELOPMENT = True
+    DEBUG = True
 
 
 # class DevelopmentConfig(Config):
