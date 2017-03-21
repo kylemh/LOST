76678a244fdda0ef3915d9d13bd630a31fb752ae
kylemh <kylemh.email@gmail.com>
2017-03-21 02:24:50 -0700

    Start activate_user.py

--- clients/activate_user.py
+++ clients/activate_user.py
@@ -2,8 +2,15 @@ import json
 import requests
 import sys
 
+if not (len(sys.argv) == 5):
+    print("ERROR: Unsported Number of Arguments")
+    print("USAGE: activate_user.py <host> <username> <password> <role>\n")
+    print("NOTE: You may only use 'facofc' or 'logofc' for the user's role.\n")
+    quit()
+
 args = {
     'username': sys.argv[2],
     'password': sys.argv[3],
     'role': sys.argv[4]
 }
+
